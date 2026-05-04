30b8bee796be4a2d0ccaa226a3dfa8e1f1c7aaeb
Juan Pablo <juanpytorresdiaz@gmail.com>
2026-05-04 19:49:43 -0300

    Problema 5 resuelto

---
+++ Dsw2026Ej8/Problema5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public abstract class Sale
+    {
+        public decimal Amount { get; set; }
+        public virtual decimal CalculateTotal() => Amount;
+    }
+
+    public class RetailSale : Sale
+    {
+        public override decimal CalculateTotal() => Amount;
+    }
+
+    public class WholesaleSale : Sale
+    {
+        public override decimal CalculateTotal() => Amount * 0.9m;
+    }
+
+    public class Problema5
+    {
+        public decimal ObtenerImporteFinal(Sale sale)
+        {
+            return sale.CalculateTotal();
+        }
+    }
+}
